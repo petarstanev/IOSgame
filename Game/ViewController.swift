5462f52f556bd14420e9af9726e788888ff67565
Petar Stanev <petarstanev@petars-mac.local>
2018-04-18 15:10:17 -0700

    Initial Commit

---
+++ Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Petar Stanev on 18/04/2018.
+//  Copyright © 2018 Petar Stanev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
